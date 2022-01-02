a7fd023010601e52b1a9845edb2a6ce12224d396
israel.neto <israel.neto@odontosystem.com.br>
2022-01-02 15:18:01 -0300

    final template v6

--- dom/js/main.js
+++ dom/js/main.js
@@ -2,7 +2,8 @@ new Vue({
   el: '#app',
   data: {
     title: 'Bom dia',
-    link: 'https://www.google.com/?client=safari&channel=mac_bm.br'
+    link: 'https://www.google.com/?client=safari&channel=mac_bm.br',
+    linkHtml: '<a href="https://www.google.com/?client=safari&channel=mac_bm.br">Google</a>'
   },
   methods: {
     greetings: function() {
