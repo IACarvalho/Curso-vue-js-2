f7adeeadc1d8e4c8c77c7def6209b7ccd69ada53
israel.neto <israel.neto@odontosystem.com.br>
2022-01-02 17:36:56 -0300

    final eventos 2

--- dom/js/eventos.js
+++ dom/js/eventos.js
@@ -2,11 +2,17 @@ new Vue({
   el: '#app',
   data: {
     title: 'Estudando eventos',
-    counter: 0
+    counter: 0,
+    x: 0,
+    y: 0
   },
   methods: {
     sum() {
       this.counter += 1
+    },
+    updatePosition(event) {
+      this.x = event.clientX
+      this.y = event.clientY
     }
   }
 })
